9f0719cb4dc7f78bb649bb2eadf09a6fb86af8eb
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-09-06 14:33:56 +0530

    Added:UC1

--- src/main/java/com/bl/java_junit/MoodAnalyser.java
+++ src/main/java/com/bl/java_junit/MoodAnalyser.java
@@ -1,7 +1,10 @@
 package com.bl.java_junit;
 
 public class MoodAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome To MoodAnalyzer");
+    public String analyseMood(String message){
+        if(message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
 }
--- src/test/java/com/bl/java_junit/MoodAnalyserTest.java
+++ src/test/java/com/bl/java_junit/MoodAnalyserTest.java
@@ -1,7 +1,22 @@
 package com.bl.java_junit;
 
+import org.junit.jupiter.api.AssertionsKt;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoodAnalyserTest {
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("Iam in Sad Mood");
+        assertEquals("SAD",mood);
+    }
+    @Test
+    public void givemMessage_WhenNotSad_ShouldReturnHappy(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("Iam in Any Mood");
+        assertEquals("HAPPY",mood);
+    }
 
 }
