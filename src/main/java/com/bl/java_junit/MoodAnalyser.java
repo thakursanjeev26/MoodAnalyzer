2c4d059fc8dd5aed81288168edddbe4ead610e3c
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-09-06 14:24:57 +0530

    Added:Welcome Message

---
+++ src/main/java/com/bl/java_junit/MoodAnalyser.java
@@ -0,0 +1,7 @@
+package com.bl.java_junit;
+
+public class MoodAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome To MoodAnalyzer");
+    }
+}
---
+++ src/test/java/com/bl/java_junit/MoodAnalyserTest.java
@@ -0,0 +1,7 @@
+package com.bl.java_junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoodAnalyserTest {
+
+}
